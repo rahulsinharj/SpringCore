b43c77fbddfcbfe14d1f1d646419bca3437fc972
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2022-01-21 17:14:03 +0530

    SpringCore1

---
+++ springcore1/src/main/java/com/springcore/Main.java
@@ -0,0 +1,21 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+		Student s1 = (Student) context.getBean("student1");
+		System.out.println(s1);
+		
+		Student s2 = (Student) context.getBean("student2");
+		System.out.println(s2);
+
+		Student s3 = (Student) context.getBean("student3");
+		System.out.println(s3);
+
+	}
+}
---
+++ springcore1/src/main/java/com/springcore/Student.java
@@ -0,0 +1,57 @@
+package com.springcore;
+
+public class Student {			// Bean class
+
+	private int studentId;
+	private String studentName;
+	private String studentAddress;
+	
+	public Student() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Student(int studentId, String studentName, String studentAddress) {
+		super();
+		this.studentId = studentId;
+		this.studentName = studentName;
+		this.studentAddress = studentAddress;
+	}
+
+	public int getStudentId() {
+		return studentId;
+	}
+
+	public void setStudentId(int studentId) {
+		System.out.println("setting studentId");
+		this.studentId = studentId;
+	}
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		System.out.println("setting studentName");
+		this.studentName = studentName;
+	}
+
+	public String getStudentAddress() {
+		return studentAddress;
+	}
+
+	public void setStudentAddress(String studentAddress) {
+		System.out.println("setting studentAddress");
+		this.studentAddress = studentAddress;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+				+ "]";
+	}
+	
+	
+	
+	
+}
