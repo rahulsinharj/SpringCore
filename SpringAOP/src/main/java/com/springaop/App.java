4c55b28f536470bb0f76cb02d268ea4a564d685f
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-05-27 00:43:07 +0530

    AOP

--- SpringAOP/src/main/java/com/springaop/App.java
+++ SpringAOP/src/main/java/com/springaop/App.java
@@ -1,26 +1,21 @@
 package com.springaop;
 
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.springaop.service.PaymentService;
 
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
-//        ApplicationContext context = new ClassPathXmlApplicationContext("com/springaop/config.xml");
-        ApplicationContext context = new ClassPathXmlApplicationContext("com/springaop/config.xml");
-        
-        PaymentService paymentObj = context.getBean("payment", PaymentService.class);
-        
-        paymentObj.makePayment(599);
-        
-        
-        
-        
-        
-        
-        
-    }
+	public static void main(String[] args) 
+	{
+//		ApplicationContext context = new AnnotationConfigApplicationContext(MyConfig.class);
+		ApplicationContext context = new ClassPathXmlApplicationContext("com/springaop/config.xml");
+
+		PaymentService paymentObj = context.getBean("payment", PaymentService.class);
+
+		paymentObj.makePayment(599);
+
+	}
 }
--- SpringAOP/src/main/java/com/springaop/aspect/MyAspect.java
+++ SpringAOP/src/main/java/com/springaop/aspect/MyAspect.java
@@ -10,13 +10,13 @@ public class MyAspect {
 	@Before("execution(* com.springaop.service.PaymentServiceImpl.makePayment(..))")		// * is the return type - accepting all return type, but we can use void, int in place of * 
 	public void printBefore() 
 	{
-		System.out.println("MyAspect BEFORE_Payment activities are been executed..");
+		System.out.println("MyAspect BEFORE_Payment activities are been executed !");
 	}
 	
 	@After("execution(* com.springaop.service.PaymentServiceImpl.makePayment(..))")		// We have to put .. in makePayment() method , because this method in PaymentServiceImpl class is accepting parameters. 
 	public void printAfter() 
 	{
-		System.out.println("MyAspect AFTER_Payment activities are been executed..");
+		System.out.println("MyAspect AFTER_Payment activities are been executed !");
 	}
 	
 }
