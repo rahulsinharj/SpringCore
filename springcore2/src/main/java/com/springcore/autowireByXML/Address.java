24bdaf1afbb6246a317b1a4b69134207e60f6e74
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-05-26 15:36:31 +0530

    SpringCore

--- springcore2/src/main/java/com/springcore/autowireByXML/Address.java
+++ springcore2/src/main/java/com/springcore/autowireByXML/Address.java
@@ -17,10 +17,11 @@ public class Address {
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	@Override
 	public String toString() {
 		return "Address [street=" + street + ", city=" + city + "]";
 	}
-	
+
 	
 }
