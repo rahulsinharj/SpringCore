a62a0f6ec56ddaa7b86b114b746ae887262476a0
rahulsinharj <rahulkumarsinha1234@gmail.com>
2022-08-27 21:36:22 +0530

    @EnableAspectJAutoProxy ensures that MyAspect will be properly processed

--- SpringAOP_2/src/main/java/com/aop/config/BeanConfig.java
+++ SpringAOP_2/src/main/java/com/aop/config/BeanConfig.java
@@ -11,3 +11,9 @@ public class BeanConfig
 {
 	
 }
+
+
+/* @EnableAspectJAutoProxy => Enables support for handling components marked with AspectJ's @Aspect annotation, 
+								similar to functionality found in Spring's <aop:aspectj-autoproxy> XML element. 
+								To be used on @Configuration classes.
+*/
